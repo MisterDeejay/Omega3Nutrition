8b6be0a89a3596a7d5b694b904df099de5ae10f6
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-04-02 09:51:11 -0700

    change from email address

--- app/mailers/pre_reg_mailer.rb
+++ app/mailers/pre_reg_mailer.rb
@@ -1,2 +1,8 @@
 class PreRegMailer < ApplicationMailer
+  default from: 'info@omegathreenutrition.com'
+
+  def pre_reg_email(user)
+    @user = user
+    mail(to: "#{@user.name} <#{@user.email}>", subject: 'Welcome to O3N!')
+  end
 end
