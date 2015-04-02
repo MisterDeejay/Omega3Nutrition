448eef6f4054f916f47a09178c0fbe95f4247acc
Khaaliq DeJan <khaaliqdejan@gmail.com>
2015-04-02 08:44:59 -0700

    generate pre_reg mailer

---
+++ app/mailers/pre_reg_mailer.rb
@@ -0,0 +1,2 @@
+class PreRegMailer < ApplicationMailer
+end
---
+++ test/mailers/pre_reg_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PreRegMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
